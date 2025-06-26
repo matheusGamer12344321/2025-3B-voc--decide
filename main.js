2e900249edc84f052e8aa8f9af12253bc592e573
matheusGamer12344321 <matheus.siebra@escola.pr.gov.br>
2025-06-26 11:02:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,41 +8,83 @@ const perguntas = [
     {
         enunciado: "Pergunta 1",
         alternativas: [
-            "alternativa 1",
-            "alternativa 2",
-        
+           {
+            texto: "alternativa 01",
+            afirmacao: "resultado 01"
+           },
+           {
+            texto: "alternativa 02",
+            afirmacao: "resultado 02"
+           }
+
         ]
     },
     {
         enunciado: "Pergunta 2",
         alternativas: [
-            "alternativa 3",
-            "alternativa 4",
-        
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado 03"
+               },
+               {
+                texto: "alternativa 04",
+                afirmacao: "resultado 04"
+               }
+
         ]
-    },{
+    }, {
         enunciado: "Pergunta 3",
         alternativas: [
-            "alternativa 5",
-            "alternativa 6",
-        
+            {
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
+               },
+               {
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
+               }
+
         ]
-    },{
+    }, {
         enunciado: "Pergunta 4",
         alternativas: [
-            "alternativa 7",
-            "alternativa 8",
-        
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+               },
+               {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+               }
+
         ]
     }
 ]
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = " ";
 
-function mostraPergunta(){
+
+function mostraPergunta() {
     perguntaAtual = perguntas[atual]
     caixaPerguntas.textContent = perguntaAtual.enunciado
+    mostraAlternativas()
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativa)
+
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    atual++
+    mostraPergunta();
 }
 
 mostraPergunta();

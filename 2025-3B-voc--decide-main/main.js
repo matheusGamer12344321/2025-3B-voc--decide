71b6a409bb5b96f130cb6c4346b588a708e37986
matheusGamer12344321 <matheus.siebra@escola.pr.gov.br>
2025-07-28 07:58:44 -0300

    Add files via upload

---
+++ 2025-3B-voc--decide-main/main.js
@@ -0,0 +1,105 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            {
+                texto: "alternativa 01",
+                afirmacao: "resultado 01"
+            },
+            {
+                texto: "alternativa 02",
+                afirmacao: "resultado 02"
+            }
+
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado 03"
+            },
+            {
+                texto: "alternativa 04",
+                afirmacao: "resultado 04"
+            }
+
+        ]
+    }, {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
+            },
+            {
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
+            }
+
+        ]
+    }, {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            }
+
+        ]
+    }
+]
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = " ";
+
+
+function mostraPergunta() {
+    if (atual >= perguntas.lenght) {
+        mostreResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textContent = perguntaAtual.enunciado
+    caixaAlternativas.textContent= ""
+    mostraAlternativas()
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativa)
+
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacao = opcaoSelecionada.afirmacao
+    historiaFinal += afirmacao + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostreResultado(){
+    caixaPerguntas.textContent = "Tudo começou...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
+
+console.log(perguntas)
